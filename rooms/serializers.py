0c04b9063749c068988b4098f9cb50168b732f4d
Chris Roth <c.roth.lambda@gmail.com>
2019-09-08 11:45:55 -0500

    again

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,7 +4,7 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     coordinates = serializers.CharField(required = False)
     name = serializers.CharField(required = False)
-    description = serializers.TextFieldrequired(required = False)
+    description = serializers.TextField(required = False)
     class Meta:
         model = Room
         fields = ('coordinates', 'id', 'name', 'description', 'n', 's', 'e', 'w' )
