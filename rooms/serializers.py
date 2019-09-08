299468bb5dfeb5af2140e03bc6061deeb479778b
Chris Roth <c.roth.lambda@gmail.com>
2019-09-08 11:37:36 -0500

    attempting put

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Room
 
 class RoomSerializer(serializers.ModelSerializer):
+    coordinates = serializers.CharField(required = False)
+    id = serializers.IntegerField(primary_key=True)
+    name = serializers.CharField(required = False)
+    description = serializers.TextFieldrequired = False()
     class Meta:
         model = Room
         fields = ('coordinates', 'id', 'name', 'description', 'n', 's', 'e', 'w' )
